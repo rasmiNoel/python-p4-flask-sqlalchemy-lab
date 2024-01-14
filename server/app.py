847fd597706ee917a2232529bf4141c410a495c3
rasmi <rasminoel@gmail.com>
2024-01-14 22:19:28 +0300

    Done with lab

--- server/app.py
+++ server/app.py
@@ -19,15 +19,68 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    
+    if not animal:
+        response_body = '<h1>Animal not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+    
+    response_body = f'''
+        <h1>{animal.name}</h1>
+        <h2>{animal.species}</h2>
+        <h3>{animal.enclosure.environment}</h3>
+        <h3>{animal.zookeeper.name}</h3>
+    '''
+    response = make_response(response_body, 200)
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    
+    if not zookeeper:
+        response_body = '<h1>Zookeeper not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+    
+    response_body = f'''
+        <h1>{zookeeper.name}</h1>
+        <h2>{zookeeper.birthday}</h2>
+    '''
+    animals = [animal for animal in zookeeper.animal]
+    
+    if not animals:
+        response_body += f'<h2>Has no animals<h2>'
+        
+    else:
+        for animal in animals:
+            response_body += f'<h2>{Animal.id}<h2>'
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    environment = Enclosure.query.filter(Enclosure.id == id).first()
+    
+    if not environment:
+        response_body = '<h1>Enclosure not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+    
+    response_body = f'''
+        <h1>{environment.environment}</h1>
+        <h2>{environment.open_to_visitors}</h2>
+    '''
+    animals = [animal for animal in environment.animal]
+    
+    if not animals:
+        response_body += f'<h2>Has no animals<h2>'
+        
+    else:
+        for animal in animals:
+            response_body += f'<h2>{Animal.id}<h2>'
+            
+    response = make_response(response_body, 200)
+    return response
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -11,13 +11,22 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.Date)
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+    animals = db.relationship('Animal', backref='enclosure')
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    zookeeper = db.Column(db.String, db.ForeignKey('zookeepers.id'))
+    enclosure = db.Column(db.String, db.ForeignKey('enclosures.id'))
